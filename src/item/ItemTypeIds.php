48feb81d4039f189fb9cd12246fb2c48221e0e54
Ayrz <dogrumucahit@hotmail.com>
2026-04-05 19:27:16 +0300

    Implemented bundles (#31)
    Introduce support for the Bundle item and its in-item storage: add Bundle item class, BundleInventory, InventoryAwareItem interface, and bundle-related sounds. Register new bundle item IDs and VanillaItems/inputs, update ItemSerializerDeserializerRegistrar mappings, and add Bundle handling to InventoryManager (dynamic inventory tracking, locating, syncing and container naming/storage). Add getUnclonedItem hooks and related inventory changes (BaseInventory, SimpleInventory, DelegateInventory, DoubleChestInventory, TransactionBuilderInventory) to avoid cloning for internal operations and to call inventory-aware item hooks appropriately. Also bump BASE_VERSION and update file headers/author tags.

--- src/item/ItemTypeIds.php
+++ src/item/ItemTypeIds.php
@@ -2,21 +2,22 @@
 
 /*
  *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *     ____            ____        __  ____
+ *    / __ )___  ___  / / /___  __/  |/  (_)___  ___
+ *   / __  / _ \/ _ \/ / __/ / / / /|_/ / / __ \/ _ \
+ *  / /_/ /  __/  __/ / /_/ /_/ / /  / / / / / /  __/
+ * /_____/\___/\___/_/\__/\__, /_/  /_/_/_/ /_/\___/
+ *                       /____/
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
- *
- *
+ * @author Ayrz
+ * @team BeeltyMine
+ * 
+ * 
  */
 
 declare(strict_types=1);
@@ -363,8 +364,25 @@ final class ItemTypeIds{
 	public const BAMBOO_HANGING_SIGN = 20324;
 	public const BAMBOO_SIGN = 20325;
 	public const ELYTRA = 20326;
+	public const BUNDLE = 20327;
+	public const WHITE_BUNDLE = 20328;
+	public const LIGHT_GRAY_BUNDLE = 20329;
+	public const GRAY_BUNDLE = 20330;
+	public const BLACK_BUNDLE = 20331;
+	public const BROWN_BUNDLE = 20332;
+	public const RED_BUNDLE = 20333;
+	public const ORANGE_BUNDLE = 20334;
+	public const YELLOW_BUNDLE = 20335;
+	public const LIME_BUNDLE = 20336;
+	public const GREEN_BUNDLE = 20337;
+	public const CYAN_BUNDLE = 20338;
+	public const LIGHT_BLUE_BUNDLE = 20339;
+	public const BLUE_BUNDLE = 20340;
+	public const PURPLE_BUNDLE = 20341;
+	public const MAGENTA_BUNDLE = 20342;
+	public const PINK_BUNDLE = 20343;
 
-	public const FIRST_UNUSED_ITEM_ID = 20327;
+	public const FIRST_UNUSED_ITEM_ID = 20344;
 
 	private static int $nextDynamicId = self::FIRST_UNUSED_ITEM_ID;
 
